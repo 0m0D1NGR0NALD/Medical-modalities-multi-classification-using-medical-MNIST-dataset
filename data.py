397f49ecacf7c172d61e8f0dbd32573d26306771
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-21 09:28:10 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from torchvision import datasets,transforms,models
+from torchvision.utils import make_grid
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
