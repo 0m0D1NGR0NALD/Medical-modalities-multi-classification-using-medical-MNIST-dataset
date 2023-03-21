498e7e11d929d81f239ea0f391745ced52413c64
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-21 10:22:17 +0300

    Update data.py

--- data.py
+++ data.py
@@ -9,4 +9,14 @@ from torchvision.utils import make_grid
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-data = "Modalities"
+data = "Modalities/"
+
+train_transform = transforms.Compose([
+    transforms.RandomRotation(10),
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+            [0.485, 0.456, 0.406],
+            [0.229, 0.224, 0.225])
+])
