809dbc587628e4d155405b3f95034967b263f776
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-25 13:25:13 +0300

    Update model.py

--- model.py
+++ model.py
@@ -13,3 +13,14 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,20)
         self.fc4 = nn.Linear(20,len(class_names))
+        
+     def forward(self,X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X,2,2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2D(X,2,2)
+        X = X.view(-1,16*54*54)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.relu(self.fc3(X))
+        X = self.fc4(X)
