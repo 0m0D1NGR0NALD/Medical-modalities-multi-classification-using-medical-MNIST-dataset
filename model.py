cd5a9b790374d6444d22c8234a9489c97e9df092
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-25 13:24:07 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,2 +1,15 @@
-from data import train_data,test_data
+from data import dataset,train_loader,test_loader
 import torch.nn as nn
+import torch.functional as F
+
+class_names = dataset.classes
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,6,3,1)
+        self.conv2 = nn.Conv2d(6,16,3,1)
+        self.fc1 = nn.Linear(16*54*54,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84,20)
+        self.fc4 = nn.Linear(20,len(class_names))
